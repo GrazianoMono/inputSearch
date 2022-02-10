a4348759bfcaeab35887d2b863e515998b6a697c
GrazianoMono <graziano@mono.studio>
2022-02-10 11:30:23 +0100

    fix: converted to use useEffect instead of onload

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,46 @@
-import react, { useState } from "react";
+import react, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import '../scss/App.scss'
+import "../scss/App.scss";
 import InputSearchList from "./InputSearchList";
-import { uniqueNamesGenerator, Config, names } from 'unique-names-generator';
+import { uniqueNamesGenerator, Config, names } from "unique-names-generator";
 
 function App() {
-    const [list, setList] = useState((localStorage.getItem('nameList').split(',') || []))
-    const [results, setResults] = useState([])
+    const [list, setList] = useState([]);
+    const [results, setResults] = useState([]);
     const config = {
-      dictionaries: [names]
-  }
+        dictionaries: [names],
+    };
 
-    function getListName () {
-      if (!list.length) return
+    useEffect(() => {
+        const names = JSON.parse(localStorage.getItem("nameList"));
+        if (names && names.length > 0) setList(names);
+        else getListNames();
+    }, []);
 
-      const listName = []
-      for (let i=0; i<500; i++) {
-        listName.push(uniqueNamesGenerator(config))
-      }
-      setList(listName)
-      localStorage.setItem('nameList', listName)
-    }
+    const getListNames = () => {
+        const listName = [];
+        for (let i = 0; i < 500; i++) {
+            listName.push(uniqueNamesGenerator(config));
+        }
+        setList(listName);
+        localStorage.setItem("nameList", JSON.stringify(listName));
+    };
 
-    function onInput (e) {
-      const query = e.target.value
-      if (!query) {
-        setResults([])
-        return
-      }
-      const queryResults = list.filter(el => {
-        if (el.includes(query)) return el
-      })
-      setResults(queryResults.slice(0, 9))
-    }
+    const onInput = (e) => {
+        const query = e.target.value;
+        if (!query) {
+            setResults([]);
+            return;
+        }
+        const queryResults = list.filter((el) => {
+            if (el.includes(query)) return el;
+        });
+        console.log(queryResults.slice(0, 9))
+        setResults(queryResults.slice(0, 9));
+    };
 
     return (
-        <div className="App" onLoad={getListName}>
+        <div className="App">
             <InputSearchList onInput={onInput} results={results} />
         </div>
     );
